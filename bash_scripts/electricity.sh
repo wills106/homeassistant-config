436226b9a5b057c692f066dd5e05c214b89092bf
wills106 <william.f.swann@googlemail.com>
2019-12-11 22:24:20 +0000

    Octopus.Energy SMART Meter Readings

---
+++ bash_scripts/electricity.sh
@@ -0,0 +1,12 @@
+STARTDATE=`date -d @$(( $(date +"%s") - 86400)) +"%Y-%m-%dT00:00:00"`
+ENDDATE=`date -d @$(( $(date +"%s") - 86400)) +"%Y-%m-%dT23:59:59"`
+STARTDATEPREV=`date -d @$(( $(date +"%s") - 172800)) +"%Y-%m-%dT00:00:00"`
+ENDDATEPREV=`date -d @$(( $(date +"%s") - 172800)) +"%Y-%m-%dT23:59:59"`
+COUNT=`curl -H "Authorization: Basic API_KEY" "https://api.octopus.energy/v1/electricity-meter-points/MPAN/meters/SERIAL/consumption/?period_from=$STARTDATE&period_to=$ENDDATE" | python -mjson.tool | grep -c "consumption"`
+
+if [[ "$COUNT" == 48 ]]
+then
+curl -H "Authorization: Basic API_KEY" "https://api.octopus.energy/v1/electricity-meter-points/MPAN/meters/SERIAL/consumption/?period_from=$STARTDATE&period_to=$ENDDATE" | jq '[.. | objects | .consumption] | add' | awk '{printf "%0.2f\n",$1}' 
+else
+curl -H "Authorization: Basic API_KEY" "https://api.octopus.energy/v1/electricity-meter-points/MPAN/meters/SERIAL/consumption/?period_from=$STARTDATEPREV&period_to=$ENDDATEPREV" | jq '[.. | objects | .consumption] | add' | awk '{printf "%0.2f\n",$1}'  
+fi
